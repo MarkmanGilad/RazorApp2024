ab2494b1c8773ab844ce21f5ca3037b9a4a4bb8c
Gilad <gilad@markman.co.il>
2024-07-21 12:31:43 +0300

    Registration2

--- RazorApp/wwwroot/js/RegExp.js
+++ RazorApp/wwwroot/js/RegExp.js
@@ -3,21 +3,22 @@ function chkText() {
     var text = document.getElementById("txt").value;
     var msg = document.getElementById("msg");
     var reg;
-    //reg = /ab/
+    //reg = /^ab$/;
     //reg = /^ab$/
-    //reg = /^[A-Z]/;
-    reg = /\s/;
+    //reg = /^[A-Za-z]+[0-9]*[A-Za-z]+$/;
+    //reg = /\s/;
     //reg = /^0\d{9}[abc]$/;
     //var str1 = "12334";
     //var i = str1.length;
-    //reg = /^0{1}(2|3|4|6|8|9|5[0|(2-8)]|73)-?[1-9]\d{6}$/; // phone
-    //reg = /^[a-zA-Z]{1}[a-zA-Z0-9]+@[a-zA-Z]{2,4}\.[a-zA-Z]{2,3}$/; 
-    //reg = /^[a-zA-Z]+[\w!@#$%^&*+?<>~-]{1,}$/;  // username
+    //reg = /^0{1}(2|3|4|6|8|9|5[0|[(2-8)]|73)-?[1-9]\d{6}$/; // phone
+    //reg = /^[a-zA-Z]{1}[a-zA-Z0-9]*@[a-zA-Z0-9\-]*[a-zA-Z]{2,4}\.[a-zA-Z]{2,3}$/;
+    //reg = /^[a-zA-Z]+[\w!@#$%^&*+?<>~-]{1,}$/;  // password
     //reg = /^[A-Za-zb0-9]+$/;
     //reg = /\D/;
-    //reg = /[a-Z]/;
+    //reg = /[a-zA-Z]/;
+    reg = /[!@#$%^&\*\+?<>~-\[\]]/;
 
-    if (reg.test(text) == true) {
+    if (reg.test(text)) {
         msg.innerHTML = "True";
         
     }
---
+++ RazorApp/wwwroot/js/Registration2.js
@@ -0,0 +1,34 @@
+﻿function formVal() {
+    var res = userNameVal();
+    res = phoneVal() && res; 
+    
+    return res;
+}
+
+
+function userNameVal() {
+    var un = document.getElementById("username");
+    var uName = un.value;
+    var msg = document.getElementById("userNameMsg");
+    
+    if (uName.length == 0) {
+        msg.innerHTML = "You must enter username";
+        return false;
+    }
+    msg.innerHTML = "";
+    return true;
+
+}
+
+function phoneVal() {
+    var ph = document.getElementById("Phone").value;
+    var msg = document.getElementById("phoneMsg");
+
+    var reg = /^0{1}(2|3|4|6|8|9|5[0|(2-8)]|73)-?[1-9]\d{6}$/;
+    if (!reg.test(ph)) {
+        msg.innerHTML = "The phone is not valid";
+        return false;
+    }
+    msg.innerHTML = "";
+    return true;
+}
