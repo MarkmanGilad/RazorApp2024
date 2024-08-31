b57d56b238c5fed391bc4cdae65e140dc31f6e44
Gilad <gilad@markman.co.il>
2024-08-31 09:28:55 +0300

    students videos

--- RazorApp/Pages/Content/FormPost.cshtml.cs
+++ RazorApp/Pages/Content/FormPost.cshtml.cs
@@ -4,19 +4,30 @@ using Microsoft.AspNetCore.Antiforgery;
 
 namespace RazorApp.Pages.Content
 {
+
     [IgnoreAntiforgeryToken(Order = 1001)] // This disables antiforgery validation
     public class FormPostModel : PageModel
     {
+        //[BindProperty]
+        //public string userName { get; set; }
+        //[BindProperty]
+        //public string pass { get; set; }
         
-        public string userName { get; set; }
-        public string pass { get; set; }
-
-        public void OnGet()
+        public IActionResult OnGet(string userName, string pass)
         {
+            //UserName = Request.Query["userName"];
+            //Pass = Request.Query["pass"];
+
+            string txt = $"{userName} {pass}";
+            return Content(txt, "text/plain");
         }
-        public void OnPost () 
-        { 
-            
+        public IActionResult OnPost (string userName, string pass) 
+        {
+            //UserName = Request.Form["userName"];
+            //Pass = Request.Form["pass"];
+
+            string txt = $"{userName} {pass}";
+            return Content(txt, "text/plain");
         }
     }
 }
